55d391af37939928f973c4f975c35d92c28d6102
Erick Garcia <erickarmandogarcia@hotmail.com>
2014-12-03 11:40:59 -0500

    Add get_home_publications method into main file.

--- main.js
+++ main.js
@@ -18,17 +18,19 @@ $(document).ready(function(){
 	};
 
 	// This is called with the results from from FB.getLoginStatus().
-	var statusChangeCallback=function(response,doPost) {
+	var statusChangeCallback=function(response) {
 		console.log(response);
 		// The response object is returned with a status field that lets the
 		// app know the current login status of the person.
 		// Full docs on the response object can be found in the documentation
 		// for FB.getLoginStatus().
 		if (response.status === 'connected') {
-			// Logged into your app and Facebook.
+			// Logged into your app and Facebook and get the data
 			getFacebookData();
+			//get all posts
 			getPosts();
-			if(doPost){ postSomething(); }
+			//get home publications
+			get_home_publications();
 		}
 		else {
 			//try to conect to facebook
@@ -37,27 +39,34 @@ $(document).ready(function(){
 	}
 
 	var  getFacebookData=function() {
-	
 		FB.api('/me',function(response){
 			$("#loginResults").html("Hello "+response.name);
 		})
 	}
 
+	/*post method example
 	var postSomething=function(){
 		console.log("posting something...");
 		FB.api('/me/feed', 'post', {message: "Fuckyou+ erick"});
+	}*/
+
+	var get_home_publications=function(){
+		FB.api("9grados/feed",'get',function(response){
+			console.log("getting home publications");
+			console.log(response);
+		})
 	}
 
 	var getPosts=function(){
-		console.log("getting posts...");
 		FB.api('me/feed','get',function(response){
+			console.log("getting posts...");
 			console.log(response);
 		})
 	}
 
 	var logIntoFacebook=function(){
 		FB.login(function(response){
-			statusChangeCallback(response,true);
+			statusChangeCallback(response);
 		},{scope:permissions});
 	}
 
